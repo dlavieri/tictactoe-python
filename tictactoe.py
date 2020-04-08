a71d4325e4cd8e70288f7e4de55b11e83335bab0
david lavieri <dvlavieri@gmail.com>
2020-04-08 13:22:24 -0700

    fix winner announcement string

--- tictactoe.py
+++ tictactoe.py
@@ -17,15 +17,15 @@ def check_for_winner():
     if board[0] != ' ' and board[0] == board[1] == board[2]:
         print(f'{board[0]} is the winner!')
     elif board[3] != ' ' and board[3] == board[4] == board[5]:
-        print(f'{board[0]} is the winner!')
+        print(f'{board[3]} is the winner!')
     elif board[6] != ' ' and board[6] == board[7] == board[8]:
-        print(f'{board[0]} is the winner!')
+        print(f'{board[6]} is the winner!')
     elif board[0] != ' ' and board[0] == board[3] == board[6]:
         print(f'{board[0]} is the winner!')
     elif board[1] != ' ' and board[1] == board[4] == board[7]:
-        print(f'{board[0]} is the winner!')
+        print(f'{board[1]} is the winner!')
     elif board[2] != ' ' and board[2] == board[5] == board[8]:
-        print(f'{board[0]} is the winner!')
+        print(f'{board[2]} is the winner!')
     elif board[0] != ' ' and board[0] == board[4] == board[8]:
         print(f'{board[0]} is the winner!')
     elif board[2] != ' ' and board[2] == board[4] == board[6]:
